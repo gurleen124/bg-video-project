ae25ef6d8c9fc9b793b1439a8e82deecac07dc03
gurleen124 <gurleen.kaur@lpinfotech.com>
2024-05-09 12:21:45 +0530

    added preloader

--- main.js
+++ main.js
@@ -2,19 +2,24 @@ let btn = document.getElementById("toggle-btn");
 let slide = btn.querySelector("span");
 let video = document.querySelector("video");
 let videoSection = document.querySelector("section");
+let preloader = document.querySelector(".preloader");
 
-btn.addEventListener('click', function(){
+window.onload = setTimeout(() => {
+    preloader.classList.add("hidden");
+}, 500);
+
+btn.addEventListener('click', function () {
     if (!slide.classList.contains("left-1/2")) {
         slide.classList.add("left-1/2");
         slide.classList.remove("left-0");
         video.pause();
     } else {
         slide.classList.remove("left-1/2");
-        slide.classList.add("left-0"); 
+        slide.classList.add("left-0");
         video.play();
     }
 })
-videoSection.addEventListener('click', function(){
+videoSection.addEventListener('click', function () {
     if (video.paused) {
         video.play();
     } else {
